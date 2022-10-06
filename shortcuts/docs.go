669b69a503d490a1a50203a22e0fd6203fff82a9
Bryan Rhee <psy2848048@gmail.com>
2022-10-07 00:12:57 +0900

    docs: added description of shortcut module

--- shortcuts/docs.go
+++ shortcuts/docs.go
@@ -1,2 +1,9 @@
-// asdf
+// The methods in this module are used for developers' UX
+//
+// The name of the methods will try to be set intuitive way to incoming Antelope developers
+// And each method would be made by combination from REST API wrapper and sign module.
+//
+// In initial phase, contract upload/check/execution, tx sign will be provided.
+// If you need more method, raising an issue and contributing to the SDK are always opened! :)
+// Don't hesitate to ask and contribute!
 package shortcuts
